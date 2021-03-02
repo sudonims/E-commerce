de84f816c9c1942ac8d75897312bef90bb6c4f25
Dnyaensh Santosh Kulkarni <dnyaneshkulkarni7077@gmail.com>
2021-03-02 15:07:22 +0530

    header fix

--- client/src/components/homepage/index.js
+++ client/src/components/homepage/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Container, fade, InputBase, makeStyles } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
+import Header from '../header.js';
 
 export default function HomePage() {
-  return <></>;
+  return <><Header /></>;
 }
