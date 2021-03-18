dc4d1c602ae2dea6c0a3f043d7f49ef9d0835425
Nimish Shah <nimishshah2000@gmail.com>
2021-03-18 15:14:29 +0530

    changes

--- client/src/components/product/product.js
+++ client/src/components/product/product.js
@@ -34,44 +34,46 @@ export default function Product({ prodId }) {
               <img src={prod.image} alt="image" />
             </Grid>
             <Grid item xs={12} sm={12} md={8} lg={8} xl={8}>
-              <div className="flex flex-col">
-                <Typography variant="h2" className="font-black">
-                  {prod.name}
-                </Typography>
-                <Typography>{prod.description}</Typography>
-                <div className="flex flex-row mt-8">
-                  <Typography variant="h5">Size</Typography>
-                  <div className="flex flex-row">
-                    {prod.sizes.map((size) => (
-                      <Avatar
-                        style={{
-                          backgroundColor: "pink",
-                          color: "black",
-                          marginLeft: 10,
-                        }}
-                      >
-                        {size}
-                      </Avatar>
-                    ))}
+              <form>
+                <div className="flex flex-col">
+                  <Typography variant="h2" className="font-black">
+                    {prod.name}
+                  </Typography>
+                  <Typography>{prod.description}</Typography>
+                  <div className="flex flex-row mt-8">
+                    <Typography variant="h5">Size</Typography>
+                    <div className="flex flex-row">
+                      {prod.sizes.map((size) => (
+                        <Avatar
+                          style={{
+                            backgroundColor: "pink",
+                            color: "black",
+                            marginLeft: 10,
+                          }}
+                        >
+                          {size}
+                        </Avatar>
+                      ))}
+                    </div>
+                  </div>
+                  <div className="flex flex-row mt-8">
+                    <Typography variant="h4" style={{ marginRight: 20 }}>
+                      Price
+                    </Typography>
+                    <Typography variant="h4">{prod.price}</Typography>
+                  </div>
+                  <div className="mt-8">
+                    <Typography variant="h4">
+                      Try the product virtually!!!
+                    </Typography>
+                    <div>
+                      <form>
+                        <input type="file" />
+                      </form>
+                    </div>
                   </div>
                 </div>
-                <div className="flex flex-row mt-8">
-                  <Typography variant="h4" style={{ marginRight: 20 }}>
-                    Price
-                  </Typography>
-                  <Typography variant="h4">{prod.price}</Typography>
-                </div>
-                <div className="mt-8">
-                  <Typography variant="h4">
-                    Try the product virtually!!!
-                  </Typography>
-                  <div>
-                    <form>
-                      <input type="file" />
-                    </form>
-                  </div>
-                </div>
-              </div>
+              </form>
             </Grid>
           </Grid>
         </Container>
