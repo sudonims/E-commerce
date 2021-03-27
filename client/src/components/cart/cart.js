fa9fe819245c917d75cb4eb62a5a3d2b8ce49544
Dnyanesh Santosh Kulkarni <55693390+dsk007-kulkarni@users.noreply.github.com>
2021-03-27 15:04:24 +0530

    cart css fix big bullshit

--- client/src/components/cart/cart.js
+++ client/src/components/cart/cart.js
@@ -38,23 +38,29 @@ export default function Cart() {
       if (item.id === id) {
         if (opr === "+") {
           item.quantity = item.quantity + 1;
-          item.effectivePrice = parseFloat((item.quantity*item.price).toFixed(2));
+          item.effectivePrice = parseFloat(
+            (item.quantity * item.price).toFixed(2)
+          );
         } else {
           if (item.quantity > 1) {
             item.quantity = item.quantity - 1;
-            item.effectivePrice = parseFloat((item.quantity*item.price).toFixed(2));
+            item.effectivePrice = parseFloat(
+              (item.quantity * item.price).toFixed(2)
+            );
           }
         }
       }
       return item;
     });
-    cart_={cart:cart_};
+    cart_ = { cart: cart_ };
     console.log(cart_);
     Cookies.set("cart", cart_);
     setCart(cart_);
   };
   React.useEffect(() => {
-    setTotalAmount(cart.cart.reduce((a, b) => a + (b["effectivePrice"] || 0), 0));
+    setTotalAmount(
+      cart.cart.reduce((a, b) => a + (b["effectivePrice"] || 0), 0)
+    );
   }, [cart]);
 
   console.log(url);
@@ -64,23 +70,38 @@ export default function Cart() {
         <Header />
         <div id="cart">
           <TableContainer>
-            <Table>
+            <Table
+              style={{
+                textAlign: "center",
+              }}
+            >
               <TableHead>
                 <TableRow>
-                  <TableCell></TableCell>
-                  <TableCell>Product</TableCell>
-                  <TableCell>Shop</TableCell>
-                  <TableCell>Price</TableCell>
-                  <TableCell></TableCell>
-                  <TableCell>Quantity</TableCell>
-                  <TableCell>Remove</TableCell>
+                  <TableCell style={{ width: "16%", textAlign: "center" }}>
+                    Image
+                  </TableCell>
+                  <TableCell style={{ width: "16%", textAlign: "center" }}>
+                    Product
+                  </TableCell>
+                  <TableCell style={{ width: "16%", textAlign: "center" }}>
+                    Shop
+                  </TableCell>
+                  <TableCell style={{ width: "16%", textAlign: "center" }}>
+                    Price
+                  </TableCell>
+                  <TableCell style={{ width: "16%", textAlign: "center" }}>
+                    Quantity
+                  </TableCell>
+                  <TableCell style={{ width: "16%", textAlign: "center" }}>
+                    Remove
+                  </TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
                 {cart.cart.map((item) => {
                   return (
                     <TableRow>
-                      <TableCell>
+                      <TableCell style={{ width: "16%", textAlign: "center" }}>
                         <img
                           height="150px"
                           width="150px"
@@ -88,21 +109,48 @@ export default function Cart() {
                           src={item.image_link}
                         />
                       </TableCell>
-                      <TableCell>{item.id}</TableCell>
-                      <TableCell> {item.name} </TableCell>
-                      <TableCell> {item.price} </TableCell>
-                      <TableCell>
-                        <div className="flex flex-row">
-                          <Button onClick={() => quan("+", item.id)}>+</Button>
+                      <TableCell style={{ width: "16%", textAlign: "center" }}>
+                        {item.id}
+                      </TableCell>
+                      <TableCell style={{ width: "16%", textAlign: "center" }}>
+                        {" "}
+                        {item.name}{" "}
+                      </TableCell>
+                      <TableCell style={{ width: "16%", textAlign: "center" }}>
+                        {" "}
+                        {item.price}{" "}
+                      </TableCell>
+                      <TableCell style={{ width: "16%", textAlign: "center" }}>
+                        <div className="flex md:flex-row flex-col justify-evenly">
+                          <Button
+                            style={{
+                              // marginRight: -40,
+                              backgroundColor: "#ff084e",
+                              color: "white",
+                            }}
+                            onClick={() => quan("+", item.id)}
+                          >
+                            +
+                          </Button>
                           <Typography>{item.quantity}</Typography>
-                          <Button onClick={() => quan("-", item.id)}>-</Button>
+                          <Button
+                            style={{
+                              // marginLeft: -40,
+                              backgroundColor: "#ff084e",
+                              color: "white",
+                            }}
+                            onClick={() => quan("-", item.id)}
+                          >
+                            -
+                          </Button>
                         </div>
                       </TableCell>
-                      <TableCell>
+                      <TableCell style={{ width: "16%", textAlign: "center" }}>
                         <form onSubmit={remove}>
                           <input name="id" hidden value={item.id} />
                           <Button
                             style={{
+                              marginTop: 25,
                               backgroundColor: "#ff084e",
                               color: "white",
                             }}
@@ -116,10 +164,14 @@ export default function Cart() {
                   );
                 })}
                 <TableRow>
-                  <TableCell>Total Cart Value: </TableCell>
+                  <TableCell style={{ width: "16%", textAlign: "center" }}>
+                    Total Cart Value:{" "}
+                  </TableCell>
                   <TableCell />
                   <TableCell />
-                  <TableCell>{totalAmount}</TableCell>
+                  <TableCell style={{ width: "16%", textAlign: "center" }}>
+                    {totalAmount}
+                  </TableCell>
                 </TableRow>
               </TableBody>
             </Table>
