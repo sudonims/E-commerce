1154274e5b5e943b60d9ff1f7a526b2daab5c60b
Dnyanesh Santosh Kulkarni <55693390+dsk007-kulkarni@users.noreply.github.com>
2021-04-05 16:32:02 +0530

    kjfbksajdbf

--- client/src/components/starters/header.js
+++ client/src/components/starters/header.js
@@ -97,7 +97,22 @@ const Profile = () => {
   const handleClick = () => {
     setOpen(!open);
   };
-
+  function uploadImage() {
+    const ref = firebase.storage().ref();
+    const file = document.querySelector("#photo").files[0];
+    const name = +new Date() + "-" + file.name;
+    const metadata = {
+      contentType: file.type
+    };
+    const task = ref.child(name).put(file, metadata);
+    task
+      .then(snapshot => snapshot.ref.getDownloadURL())
+      .then(url => {
+        console.log(url);
+        document.querySelector("#image").src = url;
+      })
+      .catch(console.error);
+  }
   return (
     <>
       <Button
@@ -294,6 +309,7 @@ export default function Header({ rightlinks, leftlinks }) {
                       .signOut()
                       .then(() => {
                         alert("SignOut Successfull");
+                        window.location.href="/";
                       });
                   }}
                   style={{
