4549b05b6d1b9012b3b454b43a9d4bc3e6da7dfd
Nimish Shah <nimishshah2000@gmail.com>
2021-05-22 17:21:00 +0530

    location

--- client/src/components/cart/checkout/AddressForm.js
+++ client/src/components/cart/checkout/AddressForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
@@ -12,8 +13,124 @@ import {
 import StepOrderContext from "./stepOrderContext";
 import AddressContext from "./addressContext";
 
-const FormCust = ({ address, classes, getAddres, setOpen }) => {
-  const { updateAddress } = React.useContext(AddressContext);
+function loadScript(src) {
+  return new Promise((resolve) => {
+    const script = document.createElement("script");
+    script.src = src;
+    script.onload = () => {
+      resolve(true);
+    };
+    script.onerror = () => {
+      resolve(false);
+    };
+    document.body.appendChild(script);
+  });
+}
+
+const FormCust = ({ classes, setOpen }) => {
+  const [position, setPosition] = React.useState(null);
+  const { address, updateAddress } = React.useContext(AddressContext);
+  const { enqueueSnackbar } = useSnackbar();
+  const history = useHistory();
+  React.useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          console.log(position);
+          setPosition({
+            latitute: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+        },
+        (err) => {
+          showError(err);
+        }
+      );
+    } else {
+      enqueueSnackbar("Geolocation is not supported by this browser!!", {
+        variant: "error",
+      });
+    }
+  }, []);
+
+  // function getLocation() {
+  //   return new Promise((resolve, reject) => {}
+  // }
+
+  function showError(error) {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        enqueueSnackbar("User denied the request for Geolocation.", {
+          variant: "error",
+        });
+        break;
+      case error.POSITION_UNAVAILABLE:
+        enqueueSnackbar("Location information is unavailable", {
+          variant: "error",
+        });
+        break;
+      case error.TIMEOUT:
+        enqueueSnackbar("The request to get user location timed out.", {
+          variant: "error",
+        });
+        break;
+      case error.UNKNOWN_ERROR:
+        enqueueSnackbar("An unknown error occurred", {
+          variant: "error",
+        });
+        break;
+      default:
+        enqueueSnackbar("Something went wrong", {
+          variant: "error",
+        });
+    }
+  }
+
+  async function getAddres() {
+    console.log(position);
+    if (!position) {
+      return;
+    } else {
+      await fetch(
+        `https://api.opencagedata.com/geocode/v1/json?q=${position.latitute}+${position.longitude}&key=79b83f97c27a423c9d683d449ef7fe46`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          // console.log(data);
+          //   {
+          //     "ISO_3166-1_alpha-2": "IN",
+          //     "ISO_3166-1_alpha-3": "IND",
+          //     "_category": "commerce",
+          //     "_type": "restaurant",
+          //     "continent": "Asia",
+          //     "country": "India",
+          //     "country_code": "in",
+          //     "house_number": "15 New Subhedar lay out nashik nagar",
+          //     "postcode": "440024",
+          //     "restaurant": "Savis Kitchen",
+          //     "state": "Maharashtra",
+          //     "state_code": "MH",
+          //     "state_district": "Jalna",
+          //     "town": "Ambad"
+          // }
+          updateAddress({
+            firstName: "",
+            lastName: "",
+            house_number: data.results[0].components.house_number,
+            address2: data.results[0].components.town,
+            town: data.results[0].components.town,
+            postcode: data.results[0].components.postcode,
+            state: data.results[0].components.state_code,
+            country: data.results[0].components.country,
+          });
+          setOpen();
+          history.push("/cart/checkout");
+        })
+        .catch((err) => alert(err));
+    }
+  }
+
   const addressSubmit = (e) => {
     e.preventDefault();
 
